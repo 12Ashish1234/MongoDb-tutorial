54a26ff887e4bdfe50abf22c659e82f6a4019392
Ashish <ashishd52280@gmail.com>
2023-03-30 12:58:15 +0530

    first commit

---
+++ src/main/java/com/programming/techie/springbootmongodbtutorial/controller/ExpenseController.java
@@ -0,0 +1,60 @@
+package com.programming.techie.springbootmongodbtutorial.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.programming.techie.springbootmongodbtutorial.model.Expense;
+import com.programming.techie.springbootmongodbtutorial.repository.ExpenseRepository;
+import com.programming.techie.springbootmongodbtutorial.service.ExpenseService;
+
+@RestController
+@RequestMapping("/api/expense")
+public class ExpenseController {
+
+	private final ExpenseService expenseService;
+
+	public ExpenseController(ExpenseService expenseService) {
+		this.expenseService = expenseService;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@PostMapping
+	public ResponseEntity addExpense(@RequestBody Expense expense) {
+		expenseService.addExpense(expense);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@SuppressWarnings("rawtypes")
+	@PutMapping
+	public ResponseEntity updateExpense(@RequestBody Expense expense) {
+		expenseService.updateExpense(expense);
+		return ResponseEntity.ok().build();
+	}
+
+	@SuppressWarnings("rawtypes")
+	@GetMapping
+	public ResponseEntity getAllExpenses() {
+		return ResponseEntity.ok(expenseService.getAllExpenses());
+
+	}
+
+	@GetMapping("/{name}")
+	public ResponseEntity<Expense> getExpenseByName(@PathVariable String name) {
+		return ResponseEntity.ok(expenseService.getExpenseByName(name));
+	}
+
+	@SuppressWarnings("rawtypes")
+	@DeleteMapping("/{id}")
+	public ResponseEntity deleteExpense(@PathVariable String id) {
+		expenseService.deleteExpense(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+}
